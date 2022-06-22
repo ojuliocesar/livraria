c6e3a62bf613924e8b65c97a6e39bc26cc5d113b
ojuliocesar <97842753+ojuliocesar@users.noreply.github.com>
2022-06-21 22:29:14 -0300

    Start js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Criar uma função em JS
+// Formato ES6 baseado em Arrow Function
+
+// Função que realiza o cadastro de livros
+const cadastrar = () => {
+    // Capitura o valor digitado no campo titulo
+    let titulo = document.getElementById('titulo').value
+
+    alert(titulo)
+}
